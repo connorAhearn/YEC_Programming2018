d6df3b493ccfdccb047c0a097ce9de5ec94a9ee9
Shyamaln Anandan <shyam123@my.yorku.ca>
2018-11-02 19:11:16 -0400

    add File Writer

--- YEC_Programming2018/src/FileReader.java
+++ YEC_Programming2018/src/FileReader.java
@@ -1,9 +1,27 @@
-import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class FileReader {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+	private String path;
+	private boolean append=false;
+	
+	public FileReader (String filePath,boolean appendVal)
+	{
+		path=filePath;
+		append =appendVal;
 	}
+	
+	public void write (String text)throws IOException 
+	{
+		FileWriter write = new FileWriter (path,append);
+		PrintWriter print = new PrintWriter(write);
+		print.printf("%s" + "%n", text );
+		print.close();
+	}
+}	
 
-}
