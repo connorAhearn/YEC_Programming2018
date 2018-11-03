92f938285a42dfb86074e98da6131d3df2441b0d
JeremyWinkler <jeremy.winkler1@gmail.com>
2018-11-02 20:59:56 -0400

    add basic JSAC

--- YEC_Programming2018/src/JSAC.java
+++ YEC_Programming2018/src/JSAC.java
@@ -4,9 +4,17 @@ import java.awt.Component;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
 public class JSAC {
 	private JFrame frame;
+	
+	public JSAC() {
+		setUpFrame();
+		addStoreData();
+	}
 	
 	private void setUpFrame() {
 		frame = new JFrame("JSAC");
@@ -17,9 +25,26 @@ public class JSAC {
 	}
 	
 	private void addStoreData() {
-		JLabel storeName = new JLabel("PROMPT");
-		storeName.setAlignmentX(Component.LEFT_ALIGNMENT);
-		frame.getContentPane().add(storeName);
+		String[] fields = {"Store Name: ", "Slogan (optional): "};
+		
+		JPanel spring = new JPanel(new SpringLayout());
+        for (int i = 0; i < fields.length; i++) {
+            JLabel l = new JLabel(fields[i], JLabel.TRAILING);
+            spring.add(l);
+            JTextField textField = new JTextField(10);
+            l.setLabelFor(textField);
+            spring.add(textField);
+        }
+        
+        SpringUtilities.makeCompactGrid(spring,
+		                fields.length, 2, //rows, cols
+		                6, 6,        //initX, initY
+		                6, 6);
+		spring.setOpaque(true);
+		JPanel name = new JPanel();
+		JPanel slogan = new JPanel();
+		spring.setAlignmentX(Component.LEFT_ALIGNMENT);
+		frame.getContentPane().add(spring);
 	}
 }
 
---
+++ YEC_Programming2018/src/JSAC_Test.java
@@ -0,0 +1,13 @@
+
+public class JSAC_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JSAC a = new JSAC();
+            }
+        });
+	}
+
+}
